23642aa98ee1641b61fcbe4347a7adf8180ad3f8
lufeee <contact@lufe.jp>
2022-07-17 02:28:41 +0900

    delete others file and make install.sh and update i3 config

---
+++ dot_zsh/install.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > git-completion.bash
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh > _git
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > git-prompt.sh
+
